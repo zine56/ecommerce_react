2c7830be03d4243af2d209df7ff3eb9120f3385f
ricardo ovando <ricardoovando04@gmail.com>
2020-10-26 08:12:05 -0300

    fix css

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDVqVlqZgIOLDBLchvMFdg2Ub4E6Qx2mjI",
-    authDomain: "ecommerereact.firebaseapp.com",
-    databaseURL: "https://ecommerereact.firebaseio.com",
-    projectId: "ecommerereact",
-    storageBucket: "ecommerereact.appspot.com",
-    messagingSenderId: "831175992002",
-    appId: "1:831175992002:web:b48d1b393c94289c2c915b"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   });
 
 const db = firebaseApp.firestore();
